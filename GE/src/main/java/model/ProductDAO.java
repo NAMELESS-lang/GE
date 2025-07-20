3fcb6c5e770c3bf4f57e02b862eaed943717afc0
matheus <matheusfiedlercolla@gmail.com>
2025-07-20 15:47:15 -0300

    Correções no DAO

--- GE/src/main/java/model/ProductDAO.java
+++ GE/src/main/java/model/ProductDAO.java
@@ -12,8 +12,8 @@ public class ProductDAO implements InterfaceProductDAO{
 	public boolean cadastrar(ConexaoDB conexao,Product produto) throws SQLException {
 		
 		
-			String query = "insert into Product(codigo_barras, nome_produto, data_validade, marca, quantidade, peso_produto, unidade, altura, largura, comprimento, valor)"+
-					"VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+			String query = "insert into product(codigo_barras, nome_produto, data_validade, marca, categoria, quantidade, peso_produto, unidade, altura, largura, comprimento, valor)"+
+					"VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
 			
 			
 			PreparedStatement state = conexao.getConn().prepareStatement(query);
@@ -22,13 +22,14 @@ public class ProductDAO implements InterfaceProductDAO{
 			state.setString(2,produto.getNomeProduto());
 			state.setDate(3, produto.getDataValidade());
 			state.setString(4, produto.getMarca());
-			state.setInt(5,produto.getQuantidade());
-			state.setDouble(6, produto.getPeso().getValorPeso());
-			state.setString(7, produto.getPeso().getMedida());
-			state.setDouble(8, produto.getDimensoes().getAltura());
-			state.setDouble(9, produto.getDimensoes().getComprimento());
-			state.setDouble(10, produto.getDimensoes().getLargura());
-			state.setDouble(11, produto.getValor());
+			state.setString(5,produto.getCategoria());
+			state.setInt(6,produto.getQuantidade());
+			state.setDouble(7, produto.getPeso().getValorPeso());
+			state.setString(8, produto.getPeso().getMedida());
+			state.setDouble(9, produto.getDimensoes().getAltura());
+			state.setDouble(10, produto.getDimensoes().getComprimento());
+			state.setDouble(11, produto.getDimensoes().getLargura());
+			state.setDouble(12, produto.getValor());
 			
 			int result = state.executeUpdate();
 			if( result != 0) {
@@ -41,7 +42,7 @@ public class ProductDAO implements InterfaceProductDAO{
 	
 	@Override
 	public boolean deletar(ConexaoDB conexao, Product produto) throws SQLException{
-			String query = "DELETE FROM Product WHERE codigo_barras = ?";
+			String query = "DELETE FROM product WHERE codigo_barras = ?";
 			
 			PreparedStatement state = conexao.getConn().prepareStatement(query);
 			state.setString(1, produto.getcodigoBarras()); 
@@ -52,8 +53,8 @@ public class ProductDAO implements InterfaceProductDAO{
 	
 	@Override
 	public boolean atualizar(ConexaoDB conexao, Product produto) throws SQLException{
-		String query = "update Product" + 
-					   "set nome_produto = ?, data_validade = ?, marca = ?, quantidade = ?, peso = ?, unidade_peso = ?, altura = ?, largura = ?, comprimento = ?, valor = ?"+
+		String query = "update product" + 
+					   "set nome_produto = ?, data_validade = ?, marca = ?, categoria = ?, quantidade = ?, peso = ?, unidade_peso = ?, altura = ?, largura = ?, comprimento = ?, valor = ?"+
 					   "where codigo_barras = ?"; 
 		
 		PreparedStatement state = conexao.getConn().prepareStatement(query);
@@ -61,14 +62,15 @@ public class ProductDAO implements InterfaceProductDAO{
 		state.setString(1, produto.getNomeProduto());
 		state.setDate(2, produto.getDataValidade());
 		state.setString(3, produto.getMarca());
-		state.setInt(4, produto.getQuantidade());
-		state.setDouble(5, produto.getPeso().getValorPeso());
-		state.setString(6, produto.getPeso().getMedida());
-		state.setDouble(7, produto.getDimensoes().getAltura());
-		state.setDouble(8, produto.getDimensoes().getLargura());
-		state.setDouble(9, produto.getDimensoes().getComprimento());
-		state.setDouble(10, produto.getValor());
-		state.setString(11,produto.getcodigoBarras());
+		state.setString(4, produto.getCategoria());
+		state.setInt(5, produto.getQuantidade());
+		state.setDouble(6, produto.getPeso().getValorPeso());
+		state.setString(7, produto.getPeso().getMedida());
+		state.setDouble(8, produto.getDimensoes().getAltura());
+		state.setDouble(9, produto.getDimensoes().getLargura());
+		state.setDouble(10, produto.getDimensoes().getComprimento());
+		state.setDouble(11, produto.getValor());
+		state.setString(12,produto.getcodigoBarras());
 		
 		int la = state.executeUpdate();
 		return la > 0 ?  true : false;
@@ -77,7 +79,7 @@ public class ProductDAO implements InterfaceProductDAO{
 	@Override
 	public ArrayList<Product> pesquisar(ConexaoDB conexao, Category category) throws SQLException{
 		List<Product> pd = new ArrayList<>();
-		String query = "SELECT * FROM Product WHERE "+ category.getCategoria()+" = ?";
+		String query = "SELECT * FROM product WHERE "+ category.getCategoria()+" = ?";
 		PreparedStatement state = conexao.getConn().prepareStatement(query);
 		state.setString(1, category.getInput());
 		
@@ -95,7 +97,7 @@ public class ProductDAO implements InterfaceProductDAO{
 
 	@Override
 	public boolean consultarCodigoBarras(ConexaoDB conexao, String codigoBarras) throws SQLException {
-		String query = "SELECT * FROM Product WHERE codigo_barras = ?";
+		String query = "SELECT * FROM product WHERE codigo_barras = ?";
 		
 		PreparedStatement state = conexao.getConn().prepareStatement(query);
 		state.setString(1, codigoBarras);
